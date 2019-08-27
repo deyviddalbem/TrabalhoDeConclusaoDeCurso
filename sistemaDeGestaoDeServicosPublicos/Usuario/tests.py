69c605689ca16c2e988f61a0ffac834d83d24f94
deyvid dalbem de souza <dalbem88@gmail.com>
2019-08-26 21:58:43 -0400

    arrumando casos de teste

--- sistemaDeGestaoDeServicosPublicos/Usuario/tests.py
+++ sistemaDeGestaoDeServicosPublicos/Usuario/tests.py
@@ -2,37 +2,37 @@ import unittest
 from selenium import webdriver
 # Create your tests here.
 
-#pip install selenium
-#python3.8 manage.py test
+# pip install selenium
+# python3.8 manage.py test
 
 
-#teste de login
+# teste de login
 class teste_login(unittest.TestCase):
-   
+
     def setUp(self):
-       self.driver=webdriver.Firefox()
-       self.driver.get('http://127.0.0.1:8000/accounts/login/')
+        self.driver = webdriver.Firefox()
+        self.driver.get('http://127.0.0.1:8000/accounts/login/')
 
-    #testando se a app está inserindo dados corretos para efetuar o login
+    # testando se a app está inserindo dados corretos para efetuar o login
     def test_login_correto(self):
-       self.usuario=self.driver.find_element_by_name("username")
-       self.usuario.send_keys("admin")
-       self.usuario=self.driver.find_element_by_name("password")
-       self.usuario.send_keys("admin")
+        self.usuario = self.driver.find_element_by_name("username")
+        self.usuario.send_keys("admin")
+        self.usuario = self.driver.find_element_by_name("password")
+        self.usuario.send_keys("admin")
 
-       self.usuario.submit()
+        self.usuario.submit()
 
-    #testando se a app está inserindo dados errados para efetuar o login
+    # testando se a app está inserindo dados errados para efetuar o login
     def teste_login_incorreto(self):
-       self.usuario=self.driver.find_element_by_name("username")
-       self.usuario.send_keys("uytret")
-       self.usuario=self.driver.find_element_by_name("password")
-       self.usuario.send_keys("hjbkço")
-       #
+        self.usuario = self.driver.find_element_by_name("username")
+        self.usuario.send_keys("uytret")
+        self.usuario = self.driver.find_element_by_name("password")
+        self.usuario.send_keys("hjbkço")
+        # bugs encontrados==>> quando o usuário digita a senha incorreta, não mostra nenhuma mensagem!
+        # bugs corrigido
 
-       self.usuario.submit()
+        self.usuario.submit()
 
     def tearDown(self):
         pass
-        #self.driver.implicitly_wait(30)
-        #self.driver.quit()
+        self.driver.quit()
