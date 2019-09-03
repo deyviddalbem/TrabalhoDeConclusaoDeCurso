11d70f7e58d253f3bfefe28cfacc2f96209a5265
deyvid dalbem de souza <dalbem88@gmail.com>
2019-09-03 08:41:53 -0400

    table tipoChamado feita

--- sistemaDeGestaoDeServicosPublicos/Chamados/admin.py
+++ sistemaDeGestaoDeServicosPublicos/Chamados/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TipoChamado
 
-# Register your models here.
+class TipoChamadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'descricaoTipoChamado',)
+    list_filter = ['id', 'descricaoTipoChamado']
+
+
+admin.site.register(TipoChamado,TipoChamadoAdmin)
--- sistemaDeGestaoDeServicosPublicos/Chamados/models.py
+++ sistemaDeGestaoDeServicosPublicos/Chamados/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TipoChamado(models.Model):
+    descricaoTipoChamado = models.CharField('TIPO CHAMADO', max_length=45)
+
+    def __str__(self):
+        return self.descricaoTipoChamado
+
+    class Meta:
+        verbose_name = "TipoChamados"
+        verbose_name_plural = "TipoChamados"
