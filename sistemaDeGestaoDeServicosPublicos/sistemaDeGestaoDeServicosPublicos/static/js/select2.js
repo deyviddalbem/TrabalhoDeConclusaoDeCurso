93202e46e6ac0bc7d621ba67369b402f75481f41
deyvid dalbem de souza <dalbem88@gmail.com>
2019-09-19 15:19:32 -0400

    select2

--- sistemaDeGestaoDeServicosPublicos/sistemaDeGestaoDeServicosPublicos/static/js/select2.js
+++ sistemaDeGestaoDeServicosPublicos/sistemaDeGestaoDeServicosPublicos/static/js/select2.js
@@ -4498,7 +4498,7 @@ S2.define('select2/i18n/en',[],function () {
       return message;
     },
     noResults: function () {
-      return 'No results found';
+      return 'Sem Resultados para a busca!';
     },
     searching: function () {
       return 'Searching…';
